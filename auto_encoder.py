fa5cb60b48f1466038e54bf4bd62ca4e642dca95
jaebyunglee <49424681+jaebyunglee@users.noreply.github.com>
2024-03-07 00:40:02 +0900

    Update auto_encoder.py

--- auto_encoder.py
+++ auto_encoder.py
@@ -1,13 +1,16 @@
 # IMPORT PAKAGES
+import math
 import datetime
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.callbacks import Callback
 
+
+
 np.random.seed(1234)
 tf.random.set_seed(1234)
-
-
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+tf.autograph.set_verbosity(0)
 
 
 # Auto Encoder Class
@@ -47,6 +50,76 @@ def custom_activation(x):
     return tf.sigmoid(x)*255
 
 
+
+class CustomAEModel() :
+
+    def __init__(self, input_shape, kernelN, kernelSize, kernelEx, strides, dropR, poolN) :
+        """
+        input_shape = (128,512,1)
+        kernelN = (8,16,64,64,16,8)
+        kernelSize = 3
+        kernelEx = 1
+        poolN = 3
+        strides = 1
+        dropR = 0
+        """
+
+        self.input_shape  = input_shape
+        self.kernelN      = kernelN
+        self.kernelSize    = kernelSize
+        self.kernelEx     = kernelEx
+        self.strides      = strides
+        self.dropR        = dropR
+        self.poolN        = poolN
+        self.build()
+
+    def ConvBlock(self, x, n_filters, kernel_size, strides) :
+        conv = tf.keras.layers.Conv2D(n_filters, kernel_size = kernel_size, strides = strides, padding = 'same')(x)
+        conv = tf.keras.layers.BatchNormalization()(conv)
+        conv = tf.keras.layers.Activation('relu')(conv)
+        return conv
+
+    def build(self) :
+        # Kernel Expand
+        if self.kernelEx == 1:
+            if self.input_shape[0] > self.input_shape[1] :
+                kk = math.ceil(self.input_shape[0]/self.input_shape[1])
+                _kernelSize = (self.kernelSize * kk , self.kernelSize)
+                
+            elif self.input_shape[0] < self.input_shape[1] :
+                kk = math.ceil(self.input_shape[1]/self.input_shape[0]) 
+                _kernelSize = (self.kernelSize , self.kernelSize * kk)
+            else :
+                _kernelSize = (self.kernelSize, self.kernelSize)
+        else :
+            _kernelSize = (self.kernelSize, self.kernelSize)
+   
+        # Encoder
+        inputs = tf.keras.layers.Input(self.input_shape)
+        enc_l = list(range(len(self.kernelN)//2))
+        for encode_range in enc_l :
+            if encode_range == 0 :
+                conv = self.ConvBlock(x = inputs, n_filters = self.kernelN[encode_range], kernel_size = _kernelSize, strides = self.strides)
+            else :
+                conv = self.ConvBlock(x = conv, n_filters = self.kernelN[encode_range], kernel_size = _kernelSize, strides = self.strides)
+
+            if encode_range in enc_l[-self.poolN:] :
+                conv = tf.keras.layers.MaxPooling2D(pool_size = (2,2))(conv)
+
+        if self.dropR > 0:
+            conv = tf.keras.layers.Dropout(self.dropR)(conv)
+
+        # Decoder
+        dec_l = list(range(len(self.kernelN)//2, len(self.kernelN)))
+        for decode_range in dec_l :
+            if decode_range in dec_l[:self.poolN] :
+                conv = tf.keras.layers.UpSampling2D(size = (2,2))(conv)
+            conv = self.ConvBlock(x = conv, n_filters = self.kernelN[decode_range], kernel_size = _kernelSize, strides = self.strides)
+
+        outputs = tf.keras.layers.Conv2D(self.input_shape[-1], 1, activation = custom_activation)(conv)
+        self.model = tf.keras.Model(inputs = inputs, outputs = outputs)
+        self.model.compile(optimizer = tf.keras.optimizers.Adam(), loss = 'mse')
+            
 
 class Unet():
     
